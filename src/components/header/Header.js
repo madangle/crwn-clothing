ae4fcbffb9f4b9bfcadee042b6248c80d36454f1
Rishikesh <rishikesh@merchloft.com>
2020-05-19 00:45:09 +0400

    added redux, user reducers and user actions

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {auth} from '../../firebase/firebase.utils';
+import {connect} from 'react-redux';
 
+import {auth} from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+
 import './Header.scss';
 
 const Header = ({currentUser}) => (
@@ -30,4 +32,8 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
