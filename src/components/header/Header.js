da47dbc5efb6a0feb64516b0306563183a2ce33c
Rishikesh <rishikesh@merchloft.com>
2020-05-19 23:37:19 +0400

    added cart icon and cart dropdwon components to app and also implemented cart reducer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,9 +5,12 @@ import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/CartIcon';
+import CartDropdown from '../cart-dropdown/CartDropdown';
+
 import './Header.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
 
     <div className="header">
         <Link className="logo-container" to="/">
@@ -28,12 +31,18 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
             }
+
+            <CartIcon/>
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }        
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser, 
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
